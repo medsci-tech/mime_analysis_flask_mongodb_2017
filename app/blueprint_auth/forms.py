29002c3e28a64ae1b56046f6bbbb44361fb6f3c4
PythonCakeMing <1851464029@qq.com>
2017-06-06 12:17:52 -0700

    models

---
+++ app/blueprint_auth/forms.py
@@ -0,0 +1,50 @@
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms.validators import DataRequired
+from wtforms.validators import Length
+from wtforms.validators import EqualTo
+
+from ..models import User
+
+
+class LoginForm(FlaskForm):
+    phone = StringField('Phone', [DataRequired(), Length(max=11)])
+    password = PasswordField('Password', [DataRequired()])
+
+    def validate(self):
+        check_validate = super(LoginForm, self).validate()
+        if not check_validate:
+            return False
+        # if the user not exist
+        user = User.query.filter_by(phone=self.phone.data).first()
+        if not user:
+            return False
+        # if the passwords not match
+        if not user.check_password(self.password.data):
+            return False
+
+        return True
+
+
+class RegisterForm(FlaskForm):
+    phone = StringField('Phone', [DataRequired(), Length(max=11)])
+    code = StringField('Code', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+    confirm = PasswordField('Confirm Password', [DataRequired(), EqualTo('password')])
+
+    def validate(self):
+        check_validate = super(RegisterForm, self).validate()
+        if not check_validate:
+            return False
+        # If the phone is allowed.
+        user = User.query.filter_by(phone=self.phone.data).first()
+        if not user:
+            return False
+        # If the code match.
+        time_diff = datetime.datetime.now() - user.updated_at
+        if (user.sms_code != self.code.data) or (time_diff.seconds > 300):
+            return False
+        return True
+
---
+++ app/models/user.py
@@ -0,0 +1,24 @@
+import datetime
+
+from . import mongodb
+from ..vendors import bcrypt
+
+
+class User(mongodb.Document):
+    phone = mongodb.StringField(required=True)
+    sms_code = mongodb.StringField(required=False)
+    password = mongodb.StringField(required=False)
+    updated_at = mongodb.DateTimeField(default=datetime.datetime.now())
+
+    meta = {'collection': 'users'}
+
+    def __repr__(self):
+        return '<User {}>'.format(self.phone)
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password)
+
+    def check_password(self, password):
+        ret = bcrypt.check_password_hash(self.password, password)
+        return ret
+
