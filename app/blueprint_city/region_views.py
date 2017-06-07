e4c27570dc5fc71bb87b30e4406513a70802cb72
PythonCakeMing <1851464029@qq.com>
2017-06-07 02:10:29 -0700

    views

--- app/blueprint_city/region_views.py
+++ app/blueprint_city/region_views.py
@@ -1,6 +1,7 @@
 import json
 
 from . import blueprint_city
+from ..models import Doctor
 from ..models import CityStatistic
 
 
@@ -36,17 +37,39 @@ def cities(province):
         'city': '$_id.city',
         'count': 1,
         '_id': 0}}
+    sort_doc = {'$sort': {'count': 1}}
+    limit_doc = {'$limit': 10}
     aggregate_list = list()
     aggregate_list.append(match_doc)
     aggregate_list.append(group_doc)
     aggregate_list.append(project_doc)
+    aggregate_list.append(sort_doc)
+    aggregate_list.append(limit_doc)
     ret = CityStatistic.objects().aggrate(*aggregate_list)
     return json.dumps(ret)
 
 
 @blueprint_city.route('/hospitals/<string:province>', methods=['GET', 'POST'])
 def hospitals(province):
-    pass
+    match_doc = {'$match': {'province': province}}
+    group_doc = {'$group': {
+        '_id': {'city': '$city'},
+        'count': {'$sum': 1}}}
+    project_doc = {'$project': {
+        'city': '$_id.city',
+        'count': 1,
+        '_id': 0}}
+    sort_doc = {'$sort': {'count': 1}}
+    limit_doc = {'$limit': 10}
+    aggregate_list = list()
+    aggregate_list.append(match_doc)
+    aggregate_list.append(group_doc)
+    aggregate_list.append(project_doc)
+    aggregate_list.append(sort_doc)
+    aggregate_list.append(limit_doc)
+    ret = Doctor.objects().aggrate(*aggregate_list)
+    return json.dumps(ret)
+
 
 
 
