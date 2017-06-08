6be1851a34f4505b8557f1d112956c8014a41999
PythonCakeMing <1851464029@qq.com>
2017-06-08 17:32:34 +0800

    city

--- app/models/city_statistic.py
+++ app/models/city_statistic.py
@@ -13,6 +13,9 @@ class CityStatistic(mongodb.Document):
 
     register_count = mongodb.IntField(required=False)
     authorize_count = mongodb.IntField(required=False)
+    live_count = mongodb.IntField(required=False)
+    open_class_count = mongodb.IntField(required=False)
+    social_circle_count = mongodb.IntField(required=False)
 
     meta = {'collection': 'city_statistics'}
 
