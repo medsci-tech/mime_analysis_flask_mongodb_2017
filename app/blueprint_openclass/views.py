a6a28c142bb240642f0a018f31f6f21e316cff74
PythonCakeMing <1851464029@qq.com>
2017-06-07 20:30:26 -0700

    openclass views

---
+++ app/blueprint_openclass/views.py
@@ -0,0 +1,12 @@
+import json
+from flask import render_template
+
+from . import blueprint_open_class
+from ..models import Doctor
+
+
+@blueprint_open_class.route('/')
+@blueprint_open_class.route('/index/')
+def index():
+    render_template('open_class/main.html')
+
