7439895df6d821a11db97dfcdaa1325faed7bfc1
PythonCakeMing <1851464029@qq.com>
2017-06-07 18:57:59 -0700

    models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,6 @@ mongodb = MongoEngine()
 # import your models here,
 # otherwise, your models won't be detected by migrate.
 from .user import User
-from .city_statistics import CityStatistic
+from .city_statistic import CityStatistic
 from .doctor import Doctor
 
--- app/models/open_class.py
+++ app/models/open_class.py
@@ -1,17 +1,36 @@
 from . import mongodb
 
 
+class OpenClassUnit(mongodb.Document):
+    unit_name = mongodb.StringField()
+    visit_count = mongodb.IntField()
+    display_count = mongodb.IntField()
+    purchase_count = mongodb.IntField()
+
+    meta = {'collection': 'open_class_units'}
+
+
 class OpenClass(mongodb.Document):
     unit_name = mongodb.StringField()
     class_name = mongodb.StringField()
-    visit_count = mongodb.IntField()
-    purchase_count = mongodb.IntField()
-    display_count = mongodb.IntField()
 
-    meta = {'collection': 'open_class_statistics'}
+    visit_count = mongodb.IntField()
+    display_count = mongodb.IntField()
+    purchase_count = mongodb.IntField()
+
+    meta = {'collection': 'open_classes'}
+
+
+class OpenClassUnitLog(mongodb.Document):
+    open_class = mongodb.ReferenceField(OpenClass)
+    year = mongodb.IntField()
+    month = mongodb.IntField()
+    day = mongodb.IntField()
 
 
 class OpenClassLog(mongodb.Document):
+    open_class = mongodb.ReferenceField(OpenClass)
+
     year = mongodb.IntField()
     month = mongodb.IntField()
     day = mongodb.IntField()
